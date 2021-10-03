ba597c49fca26ebdf8431a21bdb3532e2e33a282
Addictc <44784660+Addictc@users.noreply.github.com>
2021-10-03 18:01:12 +0200

    Update second order critieria.py

--- python/second order critieria.py
+++ python/second order critieria.py
@@ -13,25 +13,25 @@ b = diff(origin,x,y)
 c = diff(origin,x,y)
 d = diff(origin,y,y)
 
-ain = a.subs([(x,crit[0]),(y,crit[1])])
-bins = b.subs([(x,crit[0]),(y,crit[1])])
-cin = c.subs([(x,crit[0]),(y,crit[1])])
-din = d.subs([(x,crit[0]),(y,crit[1])])
+asub = a.subs([(x,crit[0]),(y,crit[1])])
+bsub = b.subs([(x,crit[0]),(y,crit[1])])
+csub = c.subs([(x,crit[0]),(y,crit[1])])
+dsub = d.subs([(x,crit[0]),(y,crit[1])])
 
 print(a,b,c,d)
 
-print(ain,bins,cin,din)
+print(asub,bsub,csub,dsub)
 
 
-det = ain*din-bins*cin
+det = asub*dsub-bsub*csub
 print("the determinant of the hesse  matrix is")
 print(det)
 
 print("the critical point is therefore")
 
-if det > 0 and ain > 0:
+if det > 0 and asub > 0:
     print("local minimum")
-elif det > 0 and ain < 0:
+elif det > 0 and asub < 0:
     print("local maximum")
 elif det < 0:
     print("saddel")
